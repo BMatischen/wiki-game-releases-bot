2eac5dcb814232489867090100dbcf2a6639baa7
BMatischen <benito.matischen01@gmail.com>
2021-09-09 13:20:00 +0100

    Activate and deactivate daily notifications in channels. Correctly get releases for last seven days and today. Check data in Month column is of expected form

--- bot.py
+++ bot.py
@@ -1,14 +1,20 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import os
 import numpy as np
 import pandas as pd
 import datetime
+import traceback
+import asyncio
 
 
 prefix = '!'
 client = commands.Bot(command_prefix = prefix)
 table_url = "http://en.wikipedia.org/wiki/{0}_in_video_games"
+
+# Subscribed channel list, stores channel and next notification dates
+channels = dict() 
+
 
 
 """ Reads wikipedia tables from article for chosen year and filters for
@@ -29,31 +35,41 @@ def get_year_data(year):
             if curr_headings == headings:
                 months.append(frame)
 
-        # Merge kept tables then remove 'TBA' release and misisng values
+        # Merge kept tables. Replace TBA dates with NaN to remove later
         df = (pd.concat(months)[['Month','Day','Title']]
-              .replace('TBA',np.NaN)
-              .dropna()
-              )
-        
+              .replace('TBA',np.NaN))
+
+        # Remove rows with inappropriate month data
+        months = ["January", "February", "March",
+                  "April", "May", "June",
+                  "July", "August", "September",
+                  "October", "November", "December"]
+        df['Month'] = df['Month'].apply(lambda x: x.title())
+        df = df[df['Month'].isin(months)].dropna()
+
         df['Title'] = df['Title'].replace(r"\[.*\]", "", regex=True)
+
+        # Add and clean columns for date data
+        # Then make string column for dates and convert column to datetime
         df['Year'] = pd.Series([year] * len(df['Month']))
         df['Day'] = df['Day'].astype(int)
-
-        # Make new string column for dates by joining release data
-        # Then convert to datetime64 and clean table
         df['Date'] = df[['Year','Month','Day']].apply(lambda x: '-'.join(x.values.astype(str)), axis='columns')
         df['Date'] = pd.to_datetime(df['Date'])
+        
         df.sort_values(by='Date', inplace=True)
         df.drop(['Year','Day','Month'], axis='columns', inplace=True)
 
-    except Exception as e:
+    except ValueError as e:
         raise e
     return df, url
+
 
 
 @client.event
 async def on_ready():
     print("Ready as {0.user}".format(client))
+    list_today.start() # Start loop for checking notifications
+
 
 
 """ List video game releases for chosen month and year,
@@ -84,6 +100,7 @@ async def list_releases(ctx, month=None, year=None):
         df = df[df['Date'].dt.strftime('%B') == curr_month]
         df['Day'] = df['Date'].dt.strftime('%d')
 
+        # Construct list of games ordered by date
         games = tuple(zip(df['Day'], df['Title']))
         msg = f"{len(games)} games released this month\n\n"
         for day, title in games:
@@ -95,16 +112,18 @@ async def list_releases(ctx, month=None, year=None):
                               color=0xFF5733)
         await ctx.send(embed=embed)
         
-    except Exception as e:
-        print(e)
+    except ValueError as e:
+        print(traceback.format_exc())
         await ctx.send("Error: Unable to get data!")
+
 
 
 """ List games released in last 7 days and on current day in an embed """
 
 @client.command(name='new', help='Show games released in past 7 days and today')
 async def post_new(ctx):
-    curr_date = ctx.message.created_at
+    curr_date = ctx.message.created_at.replace(hour=0, minute=0,
+                                               second=0, microsecond=0)
     last_date = curr_date - datetime.timedelta(days=7)
     try:
         df = get_year_data(curr_date.year)[0]
@@ -112,11 +131,18 @@ async def post_new(ctx):
             df_last = get_year_data(last_date.year)[0]
             df = pd.concat([df, df_last])
 
+        # Get releases in last 7 days and for current day
         last_week = df[df['Date'].between(last_date,
                                           curr_date,
                                           inclusive='left')]
-        today = df[df['Date'].between(curr_date, curr_date)]
 
+        mask = ((df['Date'].dt.day == curr_date.day)
+                & (df['Date'].dt.month == curr_date.month)
+                & (df['Date'].dt.year == curr_date.year)
+        )
+        today= df.where(mask).dropna()
+
+        # Construct lists of games ordered by date
         games = tuple(zip(last_week['Date'], last_week['Title']))
         msg = f"Last 7 days: {len(games)} games released\n\n"
         for date, title in games:
@@ -134,16 +160,18 @@ async def post_new(ctx):
                            color=0xFF5733)
         await ctx.send(embed=em)
 
-    except Exception as e:
-        print(e)
+    except ValueError as e:
+        print(traceback.format_exc())
         await ctx.send("Error: Unable to get data!")
+
 
 
 """ List games to be released in next 7 days in an embed """
 
 @client.command(name='soon', help="Show games releasing in the next 7 days")
 async def post_upcoming(ctx):
-    curr_date = ctx.message.created_at
+    curr_date = ctx.message.created_at.replace(hour=0, minute=0,
+                                               second=0, microsecond=0)
     end_date = curr_date + datetime.timedelta(days=7)
     try:
         df = get_year_data(curr_date.year)[0]
@@ -155,6 +183,7 @@ async def post_upcoming(ctx):
                                          end_date,
                                          inclusive='right')]
 
+        # Construct list of games ordered by date
         games = tuple(zip(upcoming['Date'], upcoming['Title']))
         msg = f"Next 7 days: {len(games)} to be released\n\n"
         for date, title in games:
@@ -167,11 +196,99 @@ async def post_upcoming(ctx):
         await ctx.send(embed=em)
 
     except Exception as e:
-        print(e)
+        print(traceback.format_exc())
         await ctx.send("Error: Unable to get data!")
 
 
 
+""" Add channel to notifcations list """
+
+@client.command(name='notify', help="Enable daily notifcations about releases in the channel this command is invoked in")
+async def notify(ctx):
+    channel = ctx.message.channel
+    curr_date = ctx.message.created_at
+    notify_date = curr_date + datetime.timedelta(hours=24)
+
+    # Wait until channel list and release data not in use
+    # before modifying channel list
+    task = asyncio.create_task(list_today())
+    await task
+    channels[channel] = notify_date
+
+    msg = f"""{ctx.message.author} has enabled daily notifications about releases in {channel}.
+To disable notifications in {channel}, use command {prefix}stop"""
+    em = discord.Embed(title="Channel Subscribed",
+                           description=msg,
+                           color=0xFF5733)
+    em.add_field(name="Next Notification",
+                 value=notify_date.strftime("%d %B %Y %H:%M %p"),
+                 inline=False)
+    await ctx.send(embed=em)
+
+
+
+""" Remove channel from notifications list """
+
+@client.command(name='stop', help="Unsubscribe channel from notifications list")
+async def remove_from_notify(ctx):
+    channel = ctx.message.channel
+
+    # Wait until channel list and release data not in use
+    # before modifying channel list
+    task = asyncio.create_task(list_today())
+    await task
+    channels.pop(channel)
+
+    msg = f"Daily notifications for {channel} disabled by {ctx.message.author}"
+    em = discord.Embed(title="Channel Unsubscribed",
+                       description=msg,
+                       color=0xFF5733)
+    await ctx.send(embed=em)
+
+
+
+""" Post embedded list of current day's releases to subscribed channels """
+
+@tasks.loop(hours=24)
+async def list_today():
+    if len(channels.keys()) > 0:
+        try:
+            # Get releases for current date
+            curr_date = datetime.datetime.now()
+            df = get_year_data(curr_date.year)[0]
+            mask = ((df['Date'].dt.day == curr_date.day)
+                    & (df['Date'].dt.month == curr_date.month)
+                    & (df['Date'].dt.year == curr_date.year)
+                    )
+            today = df.where(mask).dropna()
+
+            # Construct list of releases sorted by date
+            games = tuple(zip(today['Date'], today['Title']))
+            msg = f"{len(games)} games released\n\n"
+            for date, title in games:
+                row = f"{title}\n"
+                msg += row
+
+            # Send embedded lists to each channel where
+            # next notification date has past
+            for ch in channels.keys():
+                notify_date = channels[ch]
+                if notify_date <= curr_date:
+                    # Set new future notification date for channel
+                    notify_date = curr_date + datetime.timedelta(hours=24)
+                    channels[ch] = notify_date
+
+                    em = discord.Embed(title="Today's Releases",
+                            description=msg,
+                            color=0xFF5733)
+                    em.add_field(name="Next Notification",
+                         value=notify_date.strftime("%d %B %Y %H:%M %p"),
+                         inline=False)
+
+                    await ch.send(embed = em)
+
+        except Exception as e:
+            print(traceback.format_exc())
 
 
 
